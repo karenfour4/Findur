1cd952fd2834c962d38f0a61a1d32458c326dfe9
Karen Four <42975368+karenfour4@users.noreply.github.com>
2018-11-19 17:22:21 -0500

    Add files via upload

---
+++ four_Findur.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 19, 2018 at 05:19 PM
+-- Server version: 5.5.61-cll
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `four_Findur`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `commentId` int(11) NOT NULL,
+  `firstName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `newsletter` tinyint(1) NOT NULL,
+  `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`commentId`, `firstName`, `lastName`, `email`, `newsletter`, `comment`) VALUES
+(1, 'Tommy', 'Keeper', 'tommyk@gmail.com', 1, 'Where can I test the Find\'ur product out?');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `filesId` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patientId` int(11) NOT NULL,
+  `firstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  `height` varchar(255) NOT NULL,
+  `weight` varchar(255) NOT NULL,
+  `eyeColor` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hairColor` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `illness` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doctorName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `doctorPhone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `GPS longitude` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `GPS latitude` varchar(200) NOT NULL,
+  `emergencyContact` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `emergencyNo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`patientId`, `firstName`, `lastName`, `image`, `age`, `height`, `weight`, `eyeColor`, `hairColor`, `illness`, `doctorName`, `doctorPhone`, `GPS longitude`, `GPS latitude`, `emergencyContact`, `emergencyNo`) VALUES
+(1, 'Ralph ', 'Somebody', 'ralph.jpg', 89, '5.4', '180 lbs', 'brown', 'white', 'Dementia, Diabetic, Hypertension', 'Dr Doctor', '905983666', '', '', 'Isabelle Somebody', '9053837483'),
+(2, 'Aileen ', 'Somebody', 'aileen.jpg', 78, '5.1', '89 lbs', 'brown', 'grey', 'Altzheimers, Hypertension, Arthritis', 'Dr Doctor', '4167864321', '', '', 'Isabelle Somebody', '7848975432');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `prescriptionId` int(11) NOT NULL,
+  `drugName` text NOT NULL,
+  `dosage` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prescription`
+--
+
+INSERT INTO `prescription` (`prescriptionId`, `drugName`, `dosage`) VALUES
+(1, 'Bezafibrate', 'SR 400M'),
+(2, 'Valsartan/HCT', '80/12.5MG'),
+(3, 'Tranexamic Acid', '500MG'),
+(4, 'Naproxen Ecesome', 'P500/20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescriptionId-patientId`
+--
+
+CREATE TABLE `prescriptionId-patientId` (
+  `prescriptionId` text NOT NULL,
+  `patientId` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prescriptionId-patientId`
+--
+
+INSERT INTO `prescriptionId-patientId` (`prescriptionId`, `patientId`) VALUES
+('1', 1),
+('2', 1),
+('3', 2),
+('4', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userId-patientId`
+--
+
+CREATE TABLE `userId-patientId` (
+  `patientId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userId-patientId`
+--
+
+INSERT INTO `userId-patientId` (`patientId`, `userId`) VALUES
+(1, 1),
+(2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL,
+  `firstName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `firstName`, `lastName`, `phone`, `password`, `email`, `role`) VALUES
+(1, 'Isabelle', 'Somebody', '1234567898', 'IMM2019', 'isabelle.sombody@gmail.com', 2),
+(2, 'Karen', 'Four', '9057496938', 'Rejane@2608', 'four_user', 2),
+(3, 'Karen', 'Four', '9057496938', 'Rejane@2608', 'four_admin', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`commentId`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`filesId`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patientId`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`prescriptionId`);
+
+--
+-- Indexes for table `userId-patientId`
+--
+ALTER TABLE `userId-patientId`
+  ADD PRIMARY KEY (`patientId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `commentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `filesId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `patientId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `prescription`
+--
+ALTER TABLE `prescription`
+  MODIFY `prescriptionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `userId-patientId`
+--
+ALTER TABLE `userId-patientId`
+  MODIFY `patientId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
