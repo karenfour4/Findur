aa6477211bbf0e5eba63a80289ffe52ae2a686f9
Ikki <kaatkreative@gmail.com>
2018-12-11 10:33:57 -0500

    PUUUSH

--- locate911.php
+++ locate911.php
@@ -94,7 +94,7 @@ $row = $stmt->fetch();
 			<a href="contact.php">Contact Us</a>
 			<a href="login.php">Login</a>
 			<br />
-			<a href="index.php">Findur.com</a><a href="#" target="_blank">cookie policy</a>
+			<a href="#" target="_blank">cookie policy</a>
 			<br />
 			<form method='post' action='process-newsletter.php'>
 				Sign up for our newsletter!
